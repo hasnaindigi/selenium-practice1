1bc412a86babf9507e6c7bdd5833bcaeb0cc9731
Digital Gravity <hasnainshabbir@digitalgravity.ae>
2023-03-02 11:40:04 +0500

    1.1

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name): #yes testing
+def print_hi(name): #yes testing this
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_hi('PyCharm')
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
